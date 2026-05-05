bdf879d3018616e3aec5d6b406ef7e5a4d3e0944
Magnus <s190867@dtu.dk>
2026-05-05 15:18:36 +0200

    Fix: use ValueError instead of bare except

--- preprocess.py
+++ preprocess.py
@@ -10,27 +10,28 @@ def load_file(filename):
             value = parts[7]  #Assay_Normalized_Signal is last column
             try:
                 data[probe] = float(value)
-            except:
+            except ValueError:  #NOTE: added valueerror check to catch strictly conversion errors (e.g., for NAs)
                 continue
     return data
 
 
 def main():
 
-    #NOTE: use looping here instead of fixed paths
+    #each file below adds one time-specific data column to the final data.txt, so time imputatoin
     f1 = load_file("../22118_project_data/GSM266996.txt")
     f2 = load_file("../22118_project_data/GSM266997.txt")
     f3 = load_file("../22118_project_data/GSM266998.txt")
 
-    #keep only probes present in all files
+    #keep only probes present in all the files, so data.txt will contain only full rows
     common = set(f1) & set(f2) & set(f3)
 
-    #NOTE: added probe_id to the resultant data file
+    #added probe_id to the resultant data file
     with open("data.txt", "w") as out:
         out.write("Probe_ID\trep1\trep2\trep3\n")
 
         for probe in sorted(common):
             out.write(f"{probe}\t{f1[probe]}\t{f2[probe]}\t{f3[probe]}\n")
+
 
 if __name__ == "__main__":
     main()
